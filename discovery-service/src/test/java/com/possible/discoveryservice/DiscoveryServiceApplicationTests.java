1592a57a722ac9a7eb7f0f5643e5bc02dd476a39
Akin <olayodepossible@gmail.com>
2023-06-22 13:43:17 +0100

    project setup

---
+++ discovery-service/src/test/java/com/possible/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.possible.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
